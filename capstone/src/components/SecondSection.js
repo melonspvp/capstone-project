6215647f002d8f9b9ca76f9123922623e68e31f5
joao <melonsfort@gmail.com>
2024-07-25 14:15:35 -0300

    Second section started. Linear gradient done

--- capstone/src/components/SecondSection.js
+++ capstone/src/components/SecondSection.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./css/SecondSection.css";
 
 export default function FirstSection() {
   return (
-    <div>
-      <h1>Buceta</h1>
+    <div className="ss-main-div">
+      <div className="ss-text-div">
+        <div className="color-text-left"></div>
+        <div className="color-text-right"></div>
+        <div className="absolut-box"></div>
+      </div>
     </div>
   );
 }
