be56eba3e19bd3c50b75ff963d3dbd68306cc791
joao <melonsfort@gmail.com>
2024-07-26 14:24:17 -0300

    part 5 done. starting part 6

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -3,23 +3,17 @@ import FirstSection from "./components/FirstSection";
 import SecondSection from "./components/SecondSection";
 import ThirdSection from "./components/ThirdSection";
 import FourthSection from "./components/FourthSection";
+import Partfive from "./components/Partfive";
 import "./App.css";
 
 function App() {
   return (
     <div className="app-main-div">
-      <div className="app-fs-div">
-        <FirstSection />
-      </div>
-      <div className="app-sf-div">
-        <SecondSection />
-      </div>
-      <div className="app-sf-div">
-        <ThirdSection />
-      </div>
-      <div className="app-sf-div">
-        <FourthSection />
-      </div>
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <FourthSection />
+      <Partfive />
     </div>
   );
 }
---
+++ capstone/src/components/Partfive.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./css/Partfive.css";
+import pablo1 from "./img/pablo1.png";
+import pablo2 from "./img/pablo2.png";
+import pablo3 from "./img/pablo3.png";
+import renato1 from "./img/renato1.png";
+import renato2 from "./img/renato2.png";
+import renato3 from "./img/renato3.png";
+
+export default function Partfive() {
+    return (
+        <div className="main-div-one">
+            <h2 className="tittle">Additionally, you will have access to recorded courses and classes to complement your experience.</h2> 
+            <div className="content">
+                <h2 className="content-tittle">Mindset <span className="mentoring">Mentoring</span></h2>
+                <p className="sec-tittle">PABLO MARÇAL</p>
+                <p className="tree-tittle">1 online meeting per week</p>
+                <p className="four-tittle">MONDAY</p>
+                <div className="image-container">
+                    <img src={pablo1} alt="pablo1-image" />
+                    <img src={pablo2} alt="pablo2-image" />
+                    <img src={pablo3} alt="pablo3-image" />
+                </div>
+            </div>
+
+            <div className="content">
+                <h2 className="content-tittle">Shape <span className="mentoring">Mentoring</span></h2>
+                <p className="sec-tittle">RENATO CARIANI</p>
+                <p className="tree-tittle">1 online meeting per week</p>
+                <p className="four-tittle">WEDNESDAY</p>
+                <div className="image-container">
+                    <img src={renato1} alt="renato1-image" />
+                    <img src={renato2} alt="renato2-image" />
+                    <img src={renato3} alt="renato3-image" />
+                </div>
+            </div>
+            <div className="view-link">
+                <a href="#">I want to be MRS</a>
+            </div>
+        </div> 
+    );
+}
